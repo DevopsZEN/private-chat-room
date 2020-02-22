6d97a44ad727dbaedc098769fca3178663b9fb89
Othmane Zenzamy <59285391+DevopsZEN@users.noreply.github.com>
2020-02-22 11:47:43 +0100

    Adding on eand emit methods

--- index.js
+++ index.js
@@ -8,10 +8,17 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket) {
     console.log('a user connected');
+    
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+
     socket.on('disconnect', function() {
         console.log('User disconnected');
     });
 });
+
 
 http.listen(3000, function() {
     console.log("Listening on port 3000");
